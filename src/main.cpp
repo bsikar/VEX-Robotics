45284f22be0fb2540a9bd92d461672788046ec09
Brighton Sikarskie <brighton.sikarskie@tuta.io>
2021-02-27 17:13:19 -0800

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -21,13 +21,9 @@
  * SOFTWARE.
  */
 
-#include "math.h"
 #include "robotconfig.h"
-#include "robotstate.h"
 
 using namespace vex;
-
-const float slowDown = 0.8;
 
 void autonomous();
 void usercontrol();
@@ -44,43 +40,33 @@ void autonomous() {
 
 void usercontrol() {
     for (;;) {
-        rfdrive.spin(directionType::fwd, (Controller1.Axis3.value() - (slowDown * Controller1.Axis1.value())), velocityUnits::pct);
-        rbdrive.spin(directionType::fwd, (Controller1.Axis3.value() - (slowDown * Controller1.Axis1.value())), velocityUnits::pct);
-        lfdrive.spin(directionType::fwd, (Controller1.Axis3.value() + (slowDown * Controller1.Axis1.value())), velocityUnits::pct);
-        lbdrive.spin(directionType::fwd, (Controller1.Axis3.value() + (slowDown * Controller1.Axis1.value())), velocityUnits::pct);
-        opti.setLight(ledState::on);
-        opti.setLightPower(100, pct);
-        
-        if (Controller1.ButtonL2.pressing()) {
-            roller.setVelocity(100, percent);
-            elevator.setVelocity(80, percent);
-            if (opti.hue() > 50) {
-                elevator.spin(fwd);
-                roller.spin(reverse);
-            } else {
-                elevator.spin(forward);
-                roller.spin(forward);
-            }
-            elevator.spin(forward);
-            roller.spin(forward);
-        } else if (Controller1.ButtonL1.pressing()) {
-            elevator.spin(directionType::rev, 200, velocityUnits::pct);
-            roller.spin(directionType::rev, 200, velocityUnits::pct);
+        rfdrive.spin(directionType::fwd, Controller.Axis3.value() - Controller.Axis1.value(), velocityUnits::pct);
+        rbdrive.spin(directionType::fwd, Controller.Axis3.value() - Controller.Axis1.value(), velocityUnits::pct);
+        lfdrive.spin(directionType::fwd, Controller.Axis3.value() + Controller.Axis1.value(), velocityUnits::pct);
+        lbdrive.spin(directionType::fwd, Controller.Axis3.value() + Controller.Axis1.value(), velocityUnits::pct);
+
+        if (Controller.ButtonL1.pressing()) {
+            elevator.spin(directionType::fwd, 100, velocityUnits::pct);
+            roller.spin(directionType::fwd, 100, velocityUnits::pct);
+        } else if (Controller.ButtonL2.pressing()) {
+            elevator.spin(directionType::rev, 100, velocityUnits::pct);
+           roller.spin(directionType::rev, 100, velocityUnits::pct);
         } else {
             roller.stop(brakeType::brake);
             elevator.stop(brakeType::brake);
         }
           
-        if (Controller1.ButtonR1.pressing()) {
+        if (Controller.ButtonR1.pressing()) {
             lintake.spin(directionType::rev, 100, velocityUnits::pct);
             rintake.spin(directionType::rev, 100, velocityUnits::pct);
-        } else if (Controller1.ButtonR2.pressing()) {
+        } else if (Controller.ButtonR2.pressing()) {
             lintake.spin(directionType::fwd, 100, velocityUnits::pct);
             rintake.spin(directionType::fwd, 100, velocityUnits::pct);
         } else {
             lintake.stop(brakeType::brake);
             rintake.stop(brakeType::brake);
         }
+
         wait(15, msec);
     }
 }
